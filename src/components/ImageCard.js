e0814549bc023daac1e9a911c70cfbccd5be2937
0mikey1 <mbent015@fiu.edu>
2022-05-26 14:14:50 -0400

    final commit for simple React PhotoSearch

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -5,11 +5,22 @@ class ImageCard extends React.Component{
   constructor(props){
     super(props);
 
+    this.state ={ spans: 0 }
+
     this.imageRef = React.createRef();
   }
 
   componentDidMount(){
-    console.log(this.imageRef);
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans= () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil( height / 10);
+
+    this.setState({ spans });
+
   }
 
   render(){
@@ -18,7 +29,7 @@ class ImageCard extends React.Component{
 
 
     return(
-      <div>
+      <div style={{gridRowEnd: `span ${this.state.spans}`}}>
         <img
         ref={this.imageRef}
         alt={description}
